d24b2be16bf4368729df02eaf920d6436de1d63b
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-09-10 10:07:26 +0800

    fix build

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <>
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-V4TNY0123V"
-      strategy="afterInteractive"
-    />
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`window.dataLayer = window.dataLayer || [];
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-V4TNY0123V"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-V4TNY0123V');`}
-    </Script>
-    <Component {...pageProps} />
-  </>;
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
