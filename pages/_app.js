182d58eb5fe2449c2f9f64b29d30d5dd9071d5d9
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-09-10 10:10:53 +0800

    use next/script

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
 import "../styles/globals.css";
+import Head from 'next/head';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-V4TNY0123V"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V4TNY0123V"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-V4TNY0123V');`}
-      </Script>
+        </Script>
+      </Head>
       <Component {...pageProps} />
     </>
   );
