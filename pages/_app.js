c78b5a8d73446dfdcfe0480cb08282a7f13e090c
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-09-10 10:05:51 +0800

    update ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-V4TNY0123V"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V4TNY0123V');`}
+    </Script>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
